248b4b2c6e4699a2adf7bcfefdd53fca823f6a76
Chiemerie Ejike Mbah <ejike162@gmail.com>
2026-06-24 16:38:36 +0100

    refactor: trim password before validating

--- services/auth/authSchema.ts
+++ services/auth/authSchema.ts
@@ -3,10 +3,10 @@ import { z } from "zod";
 export const emailSignUpSchema = z.object({
   name: z.string({ error: "name is required" }).min(3),
   email: z.email({ error: "email is required" }),
-  password: z.string({ error: "Password is required" }).min(5),
+  password: z.string({ error: "Password is required" }).trim().min(5),
 });
 
 export const emailSignInSchema = z.object({
   email: z.email({ error: "email is required" }),
-  password: z.string({ error: "password is required" }).min(5),
+  password: z.string({ error: "password is required" }).trim().min(5),
 });
